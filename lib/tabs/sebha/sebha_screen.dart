b8bff601f5d7399ebf1954351df6b61361dc8999
Ezeddin Ahmed <hadouezeddin@gmail.com>
2022-07-27 01:37:14 +0000

    create sebha screen

--- lib/tabs/sebha/sebha_screen.dart
+++ lib/tabs/sebha/sebha_screen.dart
@@ -6,10 +6,10 @@ class SebhaScreen extends StatefulWidget {
 }
 
 class _SebhaScreenState extends State<SebhaScreen> {
-
-  List <String> tesbih = ['سبحان الله ', 'الحمد لله' ,'الله أكبر'];
-  int numSebha = 0 ;
-  String nameSebha = 'سبحان الله ' ;
+  List<String> tesbih = ['سبحان الله ', 'الحمد لله', 'الله أكبر'];
+  int numSebha = 0;
+  int count = 1 ;
+   String nameSebha = 'سبحان الله';
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -19,59 +19,70 @@ class _SebhaScreenState extends State<SebhaScreen> {
           children: [
             Center(
                 child: Container(
-                    margin: EdgeInsets.only(left: 50,),
-                    child: Image.asset('assets/images/headSebha.png',))),
+                    margin: EdgeInsets.only(
+                      left: 50,
+                    ),
+                    child: Image.asset(
+                      'assets/images/headSebha.png',
+                    ))),
             Container(
-                margin: EdgeInsets.only(top: 85,),
-                child: Center(child: InkWell(
-                  onTap: (){
-                    addTesbihe();
-                  },
-                    child: Image.asset('assets/images/bodySebha.png')))),
-
+                margin: EdgeInsets.only(
+                  top: 85,
+                ),
+                child: Center(
+                    child: InkWell(
+                        onTap: () {
+                          addTesbih(count);
+                        },
+                        child: Image.asset('assets/images/bodySebha.png')))),
           ],
         ),
-        SizedBox(height: 20,),
-        Text('عدد التسبيحات',
-          style: Theme.of(context).textTheme.headline2,),
-        SizedBox(height: 20,),
+        SizedBox(
+          height: 20,
+        ),
+        Text(
+          'عدد التسبيحات',
+          style: Theme.of(context).textTheme.headline2,
+        ),
+        SizedBox(
+          height: 20,
+        ),
         Container(
           height: 80,
           width: 70,
           alignment: Alignment.center,
           decoration: BoxDecoration(
               color: Theme.of(context).primaryColor,
-              borderRadius: BorderRadius.circular(25)
-          ),
-          child: Text('$numSebha',
-              style: Theme.of(context).textTheme.headline2),
+              borderRadius: BorderRadius.circular(25)),
+          child:
+              Text('$numSebha', style: Theme.of(context).textTheme.headline2),
         ),
-        SizedBox(height: 20,),
+        SizedBox(
+          height: 20,
+        ),
         Container(
           height: 50,
           width: 130,
           alignment: Alignment.center,
           decoration: BoxDecoration(
               color: Theme.of(context).primaryColor,
-              borderRadius: BorderRadius.circular(25)
-          ),
-          child: Text('$nameSebha',
-              style: Theme.of(context).textTheme.headline3),
+              borderRadius: BorderRadius.circular(25)),
+          child:
+              Text('$nameSebha', style: Theme.of(context).textTheme.headline3),
         ),
       ],
     );
   }
-  void addTesbihe(){
-    setState(() {
-      numSebha++ ;
-      if (numSebha == 34) {
-        nameSebha = tesbih[1];
-      }else if (numSebha == 67) {
-        nameSebha = tesbih[2];
-      }else if (numSebha == 100) {
-       numSebha = 0 ;
-       nameSebha = tesbih[0];
+  void addTesbih(index) {
+      numSebha++;
+      if (numSebha == 33) {
+        nameSebha = tesbih[index];
+        numSebha = 0;
+        count++ ;
+    }
+      if(nameSebha == 'الله أكبر') {
+        count = 0 ;
       }
-    });
+    setState(() {});
   }
 }
